bc8e37b6f2700ba9f3c11e0d7bf4d0b5b4bb7d07
mukhin.nick <mukhin.nick@41792619-3488-484d-a9a1-7223565fb222>
2011-11-30 09:44:04 +0000

    attached documents improved

--- ORM/src/com/xlend/orm/IPage.java
+++ ORM/src/com/xlend/orm/IPage.java
@@ -10,4 +10,6 @@ import java.sql.SQLException;
 public interface IPage {
     Object getPagescan();
     void setPagescan(Object pagescan) throws SQLException, ForeignKeyViolationException;
+    String getFileextension();
+    void setFileextension(String fileextension) throws SQLException, ForeignKeyViolationException;
 }

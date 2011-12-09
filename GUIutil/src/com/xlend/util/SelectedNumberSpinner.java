70b81ee03e2f4b4d096ef271681dff12dec311e2
mukhin.nick <mukhin.nick@41792619-3488-484d-a9a1-7223565fb222>
2011-12-09 18:04:59 +0000

    custom numeric spinner

---
+++ GUIutil/src/com/xlend/util/SelectedNumberSpinner.java
@@ -0,0 +1,55 @@
+package com.xlend.util;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.text.JTextComponent;
+
+/**
+ *
+ * @author Nick Mukhin
+ */
+public class SelectedNumberSpinner extends JSpinner {
+
+    public SelectedNumberSpinner(double initialval, double minval, double maxval, double step) {
+        super(new SpinnerNumberModel(initialval, minval, maxval, step));
+
+        ((JSpinner.DefaultEditor) getEditor()).getTextField().addFocusListener(new FocusAdapter() {
+
+            @Override
+            public void focusGained(final FocusEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+
+                    public void run() {
+                        ((JTextComponent) e.getSource()).selectAll();
+                    }
+                });
+            }
+        });
+        addChangeListener(new ChangeListener() {
+
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                setColor();
+            }
+        });
+//        setColor();
+    }
+    
+    public void setValue(Object val) {
+        super.setValue(val);
+        setColor();
+    }
+
+    private void setColor() {
+        Color color = ((Double) getValue() > 0 ? Color.BLUE : Color.RED);
+        ((JSpinner.DefaultEditor) getEditor()).getTextField().setForeground(color);
+    }
+}

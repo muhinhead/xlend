c5f49ed23a20297a93aa258ff72d309c668c87d8
mukhin.nick <mukhin.nick@41792619-3488-484d-a9a1-7223565fb222>
2012-04-13 10:01:16 +0000

    the SiteDiary form

--- XlendServer/src/com/xlend/XlendServer.java
+++ XlendServer/src/com/xlend/XlendServer.java
@@ -34,7 +34,7 @@ import javax.swing.ImageIcon;
  */
 public class XlendServer {
 
-    public static final String version = "0.31";
+    public static final String version = "0.32";
     private static final String PROPERTYFILENAME = "XlendServer.config";
     private static final String ICONNAME = "Xcost.png";
     private static Logger logger = null;

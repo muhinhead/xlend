3a992566f30466c74687bd427be9fd13f1de2f2a
mukhin.nick <mukhin.nick@41792619-3488-484d-a9a1-7223565fb222>
2012-04-22 05:19:55 +0000

    the "Bank balance" form - version Nr increased

--- XlendWorks/src/com/xlend/gui/XlendWorks.java
+++ XlendWorks/src/com/xlend/gui/XlendWorks.java
@@ -68,7 +68,7 @@ public class XlendWorks {
             return s.substring(8) + "/" + s.substring(5, 7) + "/" + s.substring(0, 4);
         }
     };
-    public static final String version = "0.33";
+    public static final String version = "0.34";
     private static Userprofile currentUser;
     private static Logger logger = null;
     private static FileHandler fh;

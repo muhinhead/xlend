37aa0a438a8e3280cf07cef984505a008e957bd5
mukhin.nick <mukhin.nick@41792619-3488-484d-a9a1-7223565fb222>
2012-05-26 06:00:59 +0000

    version 41 number

--- XlendWorks/src/com/xlend/gui/XlendWorks.java
+++ XlendWorks/src/com/xlend/gui/XlendWorks.java
@@ -48,7 +48,7 @@ public class XlendWorks {
             return s.substring(8) + "/" + s.substring(5, 7) + "/" + s.substring(0, 4);
         }
     };
-    public static final String version = "0.40";
+    public static final String version = "0.41";
     private static Userprofile currentUser;
     private static Logger logger = null;
     private static FileHandler fh;

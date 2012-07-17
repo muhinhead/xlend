6491d96967cd24d9618304040c3048a02308d017
mukhin.nick <mukhin.nick@41792619-3488-484d-a9a1-7223565fb222>
2012-07-17 15:30:44 +0000

    Filtered paged grids, active/nonactive sites

--- ORM/src/com/xlend/remote/IMessageSender.java
+++ ORM/src/com/xlend/remote/IMessageSender.java
@@ -16,6 +16,8 @@ public interface IMessageSender extends java.rmi.Remote {
     public DbObject loadDbObjectOnID(Class dbobClass, int id) 
             throws java.rmi.RemoteException;
     public Vector[] getTableBody(String select) throws java.rmi.RemoteException;
+    public Vector[] getTableBody(String select, int page, int pagesize) throws java.rmi.RemoteException;
+    public int getCount(String select) throws java.rmi.RemoteException;
     public void startTransaction(String transactionName) throws java.rmi.RemoteException;
     public void commitTransaction() throws java.rmi.RemoteException;
     public void rollbackTransaction(String transactionName) throws java.rmi.RemoteException;

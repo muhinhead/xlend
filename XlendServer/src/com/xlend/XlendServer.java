30c37dab09c0d50c800707e8921605788e9cfc1c
mukhin.nick <mukhin.nick@41792619-3488-484d-a9a1-7223565fb222>
2013-01-31 09:27:48 +0000

    work list of 25 Jan (machine service)

--- XlendServer/src/com/xlend/XlendServer.java
+++ XlendServer/src/com/xlend/XlendServer.java
@@ -46,7 +46,7 @@ import migration.MigrationDialog;
  */
 public class XlendServer {
 
-    private static final String version = "0.66.1";
+    private static final String version = "0.66.2";
     public static final String PROPERTYFILENAME = "XlendServer.config";
     private static final String ICONNAME = "Xcost.png";
     private static Logger logger = null;

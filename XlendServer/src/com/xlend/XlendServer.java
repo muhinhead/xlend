bc41725499f572f87b55509e181a53e8afd4a52a
mukhin.nick <mukhin.nick@41792619-3488-484d-a9a1-7223565fb222>
2013-02-10 12:18:25 +0000

    diesel to plant

--- XlendServer/src/com/xlend/XlendServer.java
+++ XlendServer/src/com/xlend/XlendServer.java
@@ -46,7 +46,7 @@ import migration.MigrationDialog;
  */
 public class XlendServer {
 
-    private static final String version = "0.67.0";
+    private static final String version = "0.67.2";
     public static final String PROPERTYFILENAME = "XlendServer.config";
     private static final String ICONNAME = "Xcost.png";
     private static Logger logger = null;

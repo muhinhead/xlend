8f6214e64b700b0801dce2b29e73156c17ca7b39
mukhin.nick <mukhin.nick@41792619-3488-484d-a9a1-7223565fb222>
2013-03-09 10:16:49 +0000

    printing pictures

--- XlendServer/src/com/xlend/XlendServer.java
+++ XlendServer/src/com/xlend/XlendServer.java
@@ -45,7 +45,7 @@ import migration.MigrationDialog;
  */
 public class XlendServer {
 
-    private static final String version = "0.68.a";
+    private static final String version = "0.68";
     public static final String PROPERTYFILENAME = "XlendServer.config";
     private static final String ICONNAME = "Xcost.png";
     private static Logger logger = null;

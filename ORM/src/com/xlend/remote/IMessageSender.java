2d8dc936f4f2563f1cd869f19b3b52a317baa52d
mukhin.nick <mukhin.nick@41792619-3488-484d-a9a1-7223565fb222>
2013-04-07 11:51:05 +0000

    assignments subsystem improved

--- ORM/src/com/xlend/remote/IMessageSender.java
+++ ORM/src/com/xlend/remote/IMessageSender.java
@@ -18,6 +18,7 @@ public interface IMessageSender extends java.rmi.Remote {
     public Vector[] getTableBody(String select) throws java.rmi.RemoteException;
     public Vector[] getTableBody(String select, int page, int pagesize) throws java.rmi.RemoteException;
     public int getCount(String select) throws java.rmi.RemoteException;
+    public boolean truncateTable(String tableName) throws java.rmi.RemoteException;
     public void startTransaction(String transactionName) throws java.rmi.RemoteException;
     public void commitTransaction() throws java.rmi.RemoteException;
     public void rollbackTransaction(String transactionName) throws java.rmi.RemoteException;
